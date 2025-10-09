7469b72099a55c7fc42acfbfe14277541168733a
anitamas423 <anitamasibo@gmail.com>
2025-10-09 18:06:48 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -25,14 +25,4 @@ export default defineConfig({
       '.trycloudflare.com' // allow cloudflare tunnel
     ]
   },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        about: resolve(__dirname, "src/about.html"),
-        soil: resolve(__dirname, "src/services/soil-testing.html"),
-        asphalt: resolve(__dirname, "src/services/asphalt-testing.html"),
-      },
-    },
-  }
 })
